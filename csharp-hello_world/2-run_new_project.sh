696670d40610cfa34b7e6fd03bdb03e015951311
SpStigma <yoann.rivet0@gmail.com>
2024-04-08 14:01:22 +0200

    Bash to initialize and build and run a new project C# in a folder.

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -3,4 +3,4 @@
 # Command to initialze and build and run a project inside a folder
 dotnet new console -o 2-new_project
 dotnet build ./2-new_project
-dotnet run --project ./2-new_project/1-new_project.csproj
+dotnet run --project ./2-new_project/2-new_project.csproj
