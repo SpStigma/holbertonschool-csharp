0f47468e7601735e34ad68a3665470ab015bb471
SpStigma <githubstigma@gmail.com>
2024-04-10 15:49:30 +0200

    Print number from 0 to 99 with a specifier of 2 digits min.

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int number = 0; number <= 98; number++)
+        {
+            Console.Write($"{number:00}, ");
+        }
+        Console.Write("99\n");
+    }
+}
