a38f7d325a261eff42abd8cae2962585e22eb5a5
SpStigma <githubstigma@gmail.com>
2024-04-10 15:36:26 +0200

    Fix: change in lowercase the X.

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -6,7 +6,7 @@ class Program
     {
         for (int number = 0; number <= 98; number++)
         {
-            Console.Write($"{number} = 0x{number:X}\n");
+            Console.Write($"{number} = 0x{number:x}\n");
         }
     }
 }
