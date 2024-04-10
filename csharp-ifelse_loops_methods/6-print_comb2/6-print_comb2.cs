9c178abf681ac31975523e98d0f32d7652471ad8
SpStigma <githubstigma@gmail.com>
2024-04-10 16:01:01 +0200

    Print all possible ecombinations of two digits.

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool needComma = false;
+
+        for (int number = 0; number <= 9; number++)
+        {
+            for (int secondNumber = number + 1; secondNumber <= 9; secondNumber++)
+            {
+                if (needComma)
+                {
+                    Console.Write(", ");
+                }
+                Console.Write($"{number}{secondNumber}");
+
+                needComma = true;
+            }
+        }
+        Console.WriteLine();
+    }
+}
