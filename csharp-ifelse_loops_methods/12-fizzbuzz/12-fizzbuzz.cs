dc5a43a9fbf9ac04f89ec3e6de7ffd5012e57c81
SpStigma <githubstigma@gmail.com>
2024-04-10 17:04:02 +0200

    Programm that prints the Fizz Buzz.

---
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number <= 99; number++)
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.Write("FizzBuzz ");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.Write("Fizz ");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.Write("Buzz ");
+                }
+                else
+                {
+                    Console.Write(number + " ");
+                }
+            }
+            Console.WriteLine("Buzz");
+        }
+    }
+}
