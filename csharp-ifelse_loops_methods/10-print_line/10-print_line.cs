e89c3e1c0199b1bca2dd3697bc310bdb25a8bdc7
SpStigma <githubstigma@gmail.com>
2024-04-10 16:44:47 +0200

    method that draws a straight line in the terminal.

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length <= 0)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            for (int numberToPrinted = 0; numberToPrinted < length; numberToPrinted++)
+            {
+                Console.Write("_");
+            }
+            Console.WriteLine();
+        }
+    }
+}
