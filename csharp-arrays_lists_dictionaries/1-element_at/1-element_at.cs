958dbeec156b6659a5d66af2980393244a98ba4c
SpStigma <githubstigma@gmail.com>
2024-04-15 13:42:10 +0200

    Fix: Change the way to write my condition.

--- csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -4,12 +4,11 @@ class Array
 {
     public static int elementAt(int[] array, int index)
     {
-        if (index < 0 || index >= array.Length)
+        if (index > array.Length - 1 || index < 0)
         {
-           Console.WriteLine("Index out of range");
-           return -1;
+            Console.WriteLine("Index out of range");
+            return -1;
         }
-
         return array[index];
     }
 }
