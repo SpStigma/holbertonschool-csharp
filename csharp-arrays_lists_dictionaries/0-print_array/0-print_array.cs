36e44bcd3de1a6d8684a37625f6a720a4975ae0c
SpStigma <githubstigma@gmail.com>
2024-04-15 13:23:11 +0200

    Way of Jeremy :x

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -4,28 +4,29 @@ class Array
 {
     public static int[] CreatePrint(int size)
     {
-        if(size < 0)
+        if (size < 0)
         {
             Console.WriteLine("Size cannot be negative");
             return null;
         }
-
-        if(size == 0)
+        if (size == 0)
         {
             Console.WriteLine("");
         }
-
-        int[] result = new int[size];
-        for(int i = 0; i < size; i++)
+        int [] array = new int[size];
+        for (int i = 0; i < size; i++)
         {
-            result[i] = i;
-            Console.Write(result[i]);
-            if (i < result.Length - 1)
+            array[i] = i;
+            Console.Write(array[i]);
+            if (i < array.Length - 1)
             {
                 Console.Write(' ');
             }
+            if (i == array.Length - 1)
+            {
+                Console.WriteLine();
+            }
         }
-        Console.WriteLine();
-        return result;
+        return array;
     }
 }
