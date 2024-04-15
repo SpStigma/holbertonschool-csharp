6f376141afc0071ba6b93eb6d5d6823a51723608
SpStigma <githubstigma@gmail.com>
2024-04-15 13:11:48 +0200

    Method to create an array and print it of a given size.

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if(size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        if(size == 0)
+        {
+            Console.WriteLine();
+        }
+
+        int[] result = new int[size];
+        for(int i = 0; i < size; i++)
+        {
+            result[i] = i;
+            Console.Write(result[i]);
+            if (i < result.Length - 1)
+            {
+                Console.Write(' ');
+            }
+        }
+        Console.WriteLine();
+        return result;
+    }
+}
