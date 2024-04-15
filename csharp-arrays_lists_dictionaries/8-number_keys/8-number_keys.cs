3fc5f09d83f46facd814fe41b9925fe93b589a89
SpStigma <githubstigma@gmail.com>
2024-04-15 21:14:17 +0200

    Method that returns the number of keys in a directionary.

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int iteration = 0;
+        foreach (KeyValuePair<string, string> kvp in myDict)
+        {
+            iteration++;
+        }
+        return iteration;
+    }
+}
