18f50d886dcee7fa73cf23491b67f49f7e9fd2b4
SpStigma <githubstigma@gmail.com>
2024-04-16 14:24:31 +0200

    Programm that print a two dimensional array.

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        // Create a 2D array empty of 5 * 5
+        int[,] array2D = new int[5, 5];
+
+        // initialise the case 2, 2 at 1
+        array2D[2, 2] = 1;
+
+        //go through line
+        for(int i = 0; i < array2D.GetLength(0); i++)
+        {
+            //  go through columns
+            for(int j = 0; j < array2D.GetLength(1); j++)
+            {
+                // check if the las element of the line
+                if (j < array2D.GetLength(1) - 1)
+                {
+                    Console.Write(array2D[i, j] + " ");
+                }
+                else
+                {
+                    Console.Write(array2D[i, j]);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
