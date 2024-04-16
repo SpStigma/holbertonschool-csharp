edf1037522ce1b384142b264bc90e02efb046343
SpStigma <githubstigma@gmail.com>
2024-04-16 15:34:52 +0200

    Method that returns a sorted list of common elements in two lists.

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> commonElements = new List<int>();
+        
+        // Create HashSet for fast lookup
+        HashSet<int> set1 = new HashSet<int>(list1);
+        
+        // Iterate through list2, add common elements to commonElements list
+        foreach (int num in list2)
+        {
+            if (set1.Contains(num))
+            {
+                commonElements.Add(num);
+                // Remove from set1 to avoid duplicates
+                set1.Remove(num);
+            }
+        }
+        
+        // Sort the common elements list
+        commonElements.Sort();
+        
+        return commonElements;
+    }
+}
