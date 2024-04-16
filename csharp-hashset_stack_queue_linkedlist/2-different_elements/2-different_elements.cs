c4799ddad3259cfde4c9a6bafadf2edbe4176ed4
SpStigma <githubstigma@gmail.com>
2024-04-16 15:48:50 +0200

    Method that return a sorted list of all elements present in one or the other list but not both.

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> differentElements = new List<int>();
+        
+        // Create HashSet
+        HashSet<int> set1 = new HashSet<int>(list1);
+        HashSet<int> set2 = new HashSet<int>(list2);
+        
+        // Iterate through list1, add elements not in list2 to differentElements
+        foreach (int num in list1)
+        {
+            if (!set2.Contains(num))
+            {
+                differentElements.Add(num);
+            }
+        }
+        
+        // Iterate through list2, add elements not in list1 to differentElements
+        foreach (int num in list2)
+        {
+            if (!set1.Contains(num))
+            {
+                differentElements.Add(num);
+            }
+        }
+        
+        differentElements.Sort();
+        
+        return differentElements;
+    }
+}
