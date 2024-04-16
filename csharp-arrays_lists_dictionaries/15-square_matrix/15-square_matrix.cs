02803277b48cdda94280c29abcd81f4ee6259472
SpStigma <githubstigma@gmail.com>
2024-04-16 14:34:49 +0200

    Method that computes the square of all integer of a matrix.

---
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        // Get the lenght of row and cols
+        int rows = myMatrix.GetLength(0);
+        int cols = myMatrix.GetLength(1);
+        
+        // initialise a new matrix
+        int[,] squaredMatrix = new int[rows, cols];
+        
+        // go through lines
+        for (int i = 0; i < rows; i++)
+        {
+            // go through rows
+            for (int j = 0; j < cols; j++)
+            {
+                // Squared calcul here
+                squaredMatrix[i, j] = myMatrix[i, j] * myMatrix[i, j];
+            }
+        }
+        // return the new matrix
+        return squaredMatrix;        
+    }
+}
