fcd1a39d960e4ae2ed60dff2ae7d8e8f771b6e77
SpStigma <githubstigma@gmail.com>
2024-04-16 13:23:39 +0200

    Method that returns the keys with the biggest integer value in a given value.

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int maxScore = 0;
+        string bestStudents = "";
+
+        foreach(var kvp in myList)
+        {
+            if(kvp.Value > maxScore)
+            {
+                maxScore = kvp.Value;
+                bestStudents = kvp.Key;
+            }
+        }
+        return bestStudents;
+    }
+}
