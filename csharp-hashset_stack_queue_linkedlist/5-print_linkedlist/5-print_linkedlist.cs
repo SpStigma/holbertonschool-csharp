37b2e6374c6cf7406f437e2b2ae5e27cd5c71693
SpStigma <githubstigma@gmail.com>
2024-04-17 12:07:34 +0200

    ADD : comment

--- csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -5,17 +5,20 @@ class LList
 {
     public static LinkedList<int> CreatePrint(int size)
     {
+        // Create a linkedlist empty
         LinkedList<int> list = new LinkedList<int>();
+        //Check the case
         if (size < 0)
         {
             return list;
         }
-
+        // ADD element to my linke list
         for(int i = 0; i < size; i++)
         {
             list.AddLast(i);
         }
 
+        // go through my linked list  and print it
         foreach (var item in list)
         {
             Console.WriteLine(item);
