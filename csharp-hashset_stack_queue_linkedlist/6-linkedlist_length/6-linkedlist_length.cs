ddcaf83b92974078a75bc53dd3ca927e8897f18a
SpStigma <githubstigma@gmail.com>
2024-04-17 12:14:47 +0200

    Method to return the size number of elements in a linked list.

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int size = 0;
+        foreach (var item in myLList)
+        {
+            size += 1;
+        }
+        return size;
+    }
+}
