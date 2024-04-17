f1dd3638ff2e6930582a6a7244a9a4681debf824
SpStigma <githubstigma@gmail.com>
2024-04-17 11:31:33 +0200

    method to do some interaction with a Queue.

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aQueue.Count}");
+
+        if (aQueue.Count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        else
+        {
+            Console.WriteLine($"First item: {aQueue.Peek()}");
+        }
+
+        Console.WriteLine($"Queue contains {search}: {aQueue.Contains(search)}");
+
+        if (aQueue.Contains(search))
+        {
+            while (aQueue.Count > 0)
+            {
+                if (aQueue.Dequeue() == search)
+                {
+                    break;
+                }
+            }
+        }
+
+        aQueue.Enqueue(newItem);
+
+        return aQueue;
+    }
+}
