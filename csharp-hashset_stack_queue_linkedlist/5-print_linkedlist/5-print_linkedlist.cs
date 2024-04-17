76fd0bdd68012484f995d6bbd4bbb75a7a253226
SpStigma <githubstigma@gmail.com>
2024-04-17 12:04:51 +0200

    Method that create and prints a LinkedList.

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> list = new LinkedList<int>();
+        if (size < 0)
+        {
+            return list;
+        }
+
+        for(int i = 0; i < size; i++)
+        {
+            list.AddLast(i);
+        }
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+        return list;
+    }
+}
