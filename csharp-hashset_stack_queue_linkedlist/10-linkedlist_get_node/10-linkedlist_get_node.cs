bf930958c465d04dd0a9bbc3b343ba7fb85118f7
SpStigma <githubstigma@gmail.com>
2024-04-17 13:56:03 +0200

    Method taht returns the value of the nth node of a linkedList.

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        // Check if n is valid
+        if (n < 0 ||n >= myLList.Count)
+        {
+            return 0;
+        }
+        //initialise a new node who will take the same value as our linkedList
+        LinkedListNode<int> currentNode = myLList.First;
+        //go through our linked list and update our CurrentNode
+        for (int i = 0; i < n; i++)
+        {
+            currentNode = currentNode.Next;
+        }
+        return currentNode.Value;
+    }
+}
