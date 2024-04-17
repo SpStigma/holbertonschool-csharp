3e21f9e80121abe004d45ae4708aa7781d484ba7
SpStigma <githubstigma@gmail.com>
2024-04-17 12:35:52 +0200

    Method to find a value in a linkedLinst and return his index if it's found.

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int nodePosition = 0;
+        foreach(var item in myLList)
+        {
+            if (item == value)
+            {
+                return nodePosition;
+            }
+            nodePosition++;
+        }
+        return -1;
+    }
+}
