8f1749d7bab71ff49a10c0c802a32e59d1e84753
SpStigma <githubstigma@gmail.com>
2024-05-06 12:06:19 +0200

    Public class Zombie: one field, and set to 0.

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    ///Represent the zombie class.
+    /// </summary>
+    public class Zombie
+    {
+        public int health;
+
+        ///<summary>
+        ///Constructor to set health at 0.
+        ///</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+    }
+}
