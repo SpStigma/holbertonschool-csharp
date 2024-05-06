273f38b35d47ef767972a4d755ab8a1260ef1ee1
SpStigma <githubstigma@gmail.com>
2024-05-06 14:31:00 +0200

    ADD : Overrides ToString method.

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    ///Represent the zombie class.
+    /// </summary>
+    public class Zombie
+    {
+        // health of the zombie
+        private int health;
+        // name of the zombie
+        private string name = "(No name)";
+
+        ///<summary>
+        ///Constructor to set health at 0.
+        ///</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        ///<summary>Properties of name.</summary>
+        public string Name
+        {
+            get {return name;}
+            set {name = value;}
+        }
+
+        ///<summary>
+        ///Constructor to initialize health to a specified value.
+        ///</summary>
+        public Zombie(int value)
+        {
+
+            if(value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+        ///<summary>Return the health oh the zombie.</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        ///<summary>Overrides the ToString method to print the Zombie object's attributes.</summary>
+        public override string ToString()
+        {
+            return "Zombie name: " + name + " \\ Total Health: " + health;
+        }
+    }
+}
+
+
+
