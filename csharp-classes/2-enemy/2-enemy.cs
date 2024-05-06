771f08739b69bfd9e8558fc965598aea885fb44c
SpStigma <githubstigma@gmail.com>
2024-05-06 12:29:46 +0200

    New constructor to set health in a specified value > 0.

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    ///Represent the zombie class.
+    /// </summary>
+    public class Zombie
+    {
+        ///<summary>Represent the field health.</summary>
+        public int health;
+
+        ///<summary>
+        ///Constructor to set health at 0.
+        ///</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        ///<summary>
+        ///Constructor to initialize health to a specified value.
+        ///</summary>
+        public Zombie(int value)
+        {
+
+            if(value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+    }
+}
+
