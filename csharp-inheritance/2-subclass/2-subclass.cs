1e7fc1d55194b2572ecf437814e3935154482cdc
Rivet Yoann <GitHubStigma@gmail.com>
2024-05-13 22:26:23 +0200

    Know when a class is a Subclass of a class.

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Represent the obj class.
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Method to return True if it is a Subclass.
+    /// </summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return derivedType.IsSubclassOf(baseType);  
+    }
+}
