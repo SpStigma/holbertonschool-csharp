dc42a77ca21cd251781b6503e3e2ebb6f50e6839
Rivet Yoann <GitHubStigma@gmail.com>
2024-05-13 14:24:20 +0200

    Method that return True if is an instance of a class or inherited from Array.

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Represent the Obj class.
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Represent the method to return true of false if is type s Array or inherited from Array.
+    /// </summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+}
