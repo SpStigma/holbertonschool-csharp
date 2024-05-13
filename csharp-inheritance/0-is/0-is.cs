2f55199f1bac8b19a1c639782a1d4a57420e3069
Rivet Yoann <GitHubStigma@gmail.com>
2024-05-13 14:07:02 +0200

    Method that return true of false on a specified Type.

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Represent the class obj.
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Method that return true or false if is type int.
+    /// </summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        return obj is int;
+    }
+}
+
