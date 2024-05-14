8b877ebd2952df4b9fd14c92ef7a00b8379b2b49
Rivet Yoann <134633530+SpStigma@users.noreply.github.com>
2024-05-14 12:49:21 +0200

    Creation of a class Rectangle that inherits from shape with some getand set.

---
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/// <summary>
+/// Represents the Shape class.
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Method that throws an exception.
+    /// </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0.");
+            width = value;
+        }
+    }
+
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0.");
+            height = value;
+        }
+    }
+}
