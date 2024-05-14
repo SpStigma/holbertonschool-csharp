e333862c265f90b4d5f6fcedf75d878567b45355
Rivet Yoann <134633530+SpStigma@users.noreply.github.com>
2024-05-14 12:34:30 +0200

    Class Shape with a throw execption.

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Represent The Shape class.
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Method taht throw an exeption.
+    /// </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
