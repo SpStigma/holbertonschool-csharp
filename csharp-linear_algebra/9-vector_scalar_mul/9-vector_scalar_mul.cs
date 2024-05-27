f77634e2f7800797518f80556979717d100c2866
Rivet Yoann <GitHubStigma@gmail.com>
2024-05-27 15:15:19 +0200

    Task 9: Method that multiply a vector by a scolar.

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary>
+/// Represent the VectorMath.
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Method that Vector-scalar multiplication.
+    /// </summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length != 2 && vector.Length != 3)
+        {
+            return new double[] {-1};
+        }
+
+        double[] newVector = new double[vector.Length];
+
+        for (int i = 0; i < vector.Length; i++)
+        {
+            newVector[i] = vector[i] * scalar;
+        }
+
+        return newVector;
+    }
+}
