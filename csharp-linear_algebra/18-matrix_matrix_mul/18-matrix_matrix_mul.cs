bb07df0a7357c86c352babacf027c6f1b72206a9
Rivet Yoann <GitHubStigma@gmail.com>
2024-05-27 17:04:45 +0200

    Method to multiplies two matrices and returns the new matrix.

---
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+/// <summary>
+/// Represent the MatrixMath class.
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Method to multiply 2 matrix.
+    /// </summary>
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        int colsMatrix1 = matrix1.GetLength(1);
+        int colsMatrix1 = matrix1.GetLength(1);
+        int rowsMatrix2 = matrix2.GetLength(0);
+        int colsMatrix2 = matrix2.GetLength(1);
+
+        if (colsMatrix1 != rowsMatrix2)
+        {
+            return new double[,] {{-1}};
+        }
+
+        double[,] result = new double[rowsMatrix1, colsMatrix2];
+
+        // Go through rows Matrix1
+        for (int i = 0; i < rowsMatrix1; i++)
+        {
+            // Go through cols Matrix2
+            for(int j = 0; j < colsMatrix2; j++)
+            {
+                double sum = 0;
+                //go through col Matrix1
+                for (int k = 0; k < colsMatrix1; k++)
+                {
+                    // sum of number of position i, k multiplie by the position of matrix2 k, j.
+                    sum += matrix1[i, k] * matrix2[k, j];
+                }
+                result[i, j] = sum;
+            }
+        }
+        return result;
+    }
+}
+
