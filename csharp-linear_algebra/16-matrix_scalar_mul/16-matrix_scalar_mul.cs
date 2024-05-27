a284195cd9834029b16b39958865b15a80f2b432
Rivet Yoann <GitHubStigma@gmail.com>
2024-05-27 16:20:27 +0200

    Fix: Change the way to check if it is a 2d or 3d.

--- csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -11,13 +11,14 @@ class MatrixMath
     /// </summary>
     public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
-        if(matrix.GetLength(0) != 3 && matrix.GetLength(0) != 2)
-        {
-            return new double[,] {{-1}};
-        }
-
         int rows = matrix.GetLength(0);
         int cols = matrix.GetLength(1);
+
+        if (rows < 1 || cols < 1 || rows > 3 || cols > 3)
+        {
+            return new double[,] { { -1 } };
+        }
+
         double[,] result = new double[rows, cols];
 
         for(int i = 0; i < rows; i++)
