7244ef7a8f9c89adead7336d2a49d6066f09e503
Rivet Yoann <GitHubStigma@gmail.com>
2024-05-27 15:34:06 +0200

    Method to calculate dot product of either 2D or 3D.

---
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/// <summary>
+/// Represent VectorMath class.
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Method to return Dot product of either 2D or 3D vectors.
+    /// </summary>
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        if ((vector1.Length != 2 && vector1.Length != 3) || (vector2.Length != 2 && vector2.Length != 3))
+        {
+            return -1;
+        }
+
+        if (vector1.Length != vector2.Length)
+        {
+            return -1;
+        }
+
+        double dotProduct = 0;
+
+        for (int i = 0; i < vector1.Length; i++)
+        {
+            dotProduct += vector1[i] * vector2[i];
+        } 
+        return dotProduct;
+    }
+}
+
