f07729b0d5cfc1642768e984a399ca345e316da6
Rivet Yoann <GitHubStigma@gmail.com>
2024-05-27 13:49:33 +0200

    Method to calculate and returns the lenght of a vector.

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length != 2 || vector.Length != 3)
+        {
+            return -1;
+        }
+
+        double result = 0;
+
+        foreach (double coordonates in vector)
+        {
+            result += coordonates * coordonates;
+        }
+
+        return Math.Round(Math.Sqrt(result), 2);
+    }
+}
