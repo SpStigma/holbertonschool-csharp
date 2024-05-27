2689c4966fe71ddfdb0f600627c4953dc151de2c
Rivet Yoann <134633530+SpStigma@users.noreply.github.com>
2024-05-27 22:52:55 +0200

    Correction of the formule.

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -29,17 +29,17 @@ class MatrixMath
 
         if (angle > 0)
         {
-            result[0, 0] = cos * matrix[0, 0] - sin * matrix[0, 1];
+            result[0, 0] = cos * matrix[0, 0] + (-1 * sin) * matrix[0, 1];
             result[0, 1] = sin * matrix[0, 0] + cos * matrix[0, 1];
-            result[1, 0] = cos * matrix[1, 0] - sin * matrix[1, 1];
+            result[1, 0] = cos * matrix[1, 0] + (-1 * sin) * matrix[1, 1];
             result[1, 1] = sin * matrix[1, 0] + cos * matrix[1, 1];
         }
         else
         {
             result[0, 0] = cos * matrix[0, 0] + sin * matrix[0, 1];
-            result[0, 1] = -sin * matrix[0, 0] + cos * matrix[0, 1];
+            result[0, 1] = (-1 * sin) * matrix[0, 0] + cos * matrix[0, 1];
             result[1, 0] = cos * matrix[1, 0] + sin * matrix[1, 1];
-            result[1, 1] = -sin * matrix[1, 0] + cos * matrix[1, 1];
+            result[1, 1] = (-1 * sin) * matrix[1, 0] + cos * matrix[1, 1];
         }
 
         return result;
