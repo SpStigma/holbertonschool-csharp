7a58526c7045526585b0c33f8a9e65ca00e26e8e
Rivet Yoann <134633530+SpStigma@users.noreply.github.com>
2024-05-27 22:05:39 +0200

    Add cols and change the way of positiv negative.

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -11,31 +11,34 @@ class MatrixMath
     public static double[,] Rotate2D(double[,] matrix, double angle)
     {
         int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
         double cos = Math.Cos(angle);
         double sin = Math.Sin(angle);
 
-        if (rows != 2 || rows != 2)
+        if (rows != 2 || cols != 2)
         {
-            return new double[,] {{-1}};
+            return new double[,] { { -1 } };
         }
 
-        double[,] result = new double[2, 2];
+        double[,] result = new double[rows, cols];
 
-        if (angle < 0)
+        if (angle >= 0)
+        {
+            result[0, 0] = cos * matrix[0, 0] - sin * matrix[0, 1];
+            result[0, 1] = sin * matrix[0, 0] + cos * matrix[0, 1];
+            result[1, 0] = cos * matrix[1, 0] - sin * matrix[1, 1];
+            result[1, 1] = sin * matrix[1, 0] + cos * matrix[1, 1];
+        }
+        else
         {
             result[0, 0] = cos * matrix[0, 0] + sin * matrix[0, 1];
             result[0, 1] = -sin * matrix[0, 0] + cos * matrix[0, 1];
             result[1, 0] = cos * matrix[1, 0] + sin * matrix[1, 1];
             result[1, 1] = -sin * matrix[1, 0] + cos * matrix[1, 1];
         }
-        else
-        {
-            result[0, 0] = cos * matrix[0, 0] + -sin * matrix[0, 1];
-            result[0, 1] = sin * matrix[0, 0] + cos * matrix[0, 1];
-            result[1, 0] = cos * matrix[1, 0] + -sin * matrix[1, 1];
-            result[1, 1] = sin * matrix[1, 0] + cos * matrix[1, 1];
-        }
+
         return result;
     }
+}
 }
 
