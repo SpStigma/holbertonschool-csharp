3093651d5073ee6c300aba2f61452b84a8d9ce6e
Rivet Yoann <134633530+SpStigma@users.noreply.github.com>
2024-05-28 00:13:36 +0200

    Method that shears a square 2D matrix by a given shear factor.

---
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+/// <summary>
+/// Represent the MatrixMath
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Represent a Matrix Shear Method.
+    /// </summary>
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        if (direction != 'x' && direction != 'y')
+        {
+            return new double[,] {{-1}};
+        }
+
+        if (matrix.GetLength(0) != 2)
+        {
+            return new double[,] {{-1}};
+        }
+
+        double[,] result = new double[2, 2];
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        if (direction == 'x')
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                result[i, 0] = matrix[i, 0] + factor * matrix[i, 1];
+                result[i, 1] = matrix[i , 1];
+            }
+        }
+        else if (direction == 'y')
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                result[i, 0] = matrix[i, 0];
+                result[i , 1] = factor * matrix[i, 0] + matrix[i, 1];
+            }
+        }
+
+        return result;
+    }
+}
