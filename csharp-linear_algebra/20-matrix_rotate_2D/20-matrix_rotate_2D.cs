abe84cd34ef128ef9c8be7900be0f0a9e4d60891
Rivet Yoann <134633530+SpStigma@users.noreply.github.com>
2024-05-27 21:37:39 +0200

    Task 20: method that rotates a square 2D matrix by a given angle in radians and returns the resulting matrix.

---
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/// <summary>
+/// Represent the MatrixMath class.
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Method that apply a Matrix Rotation.
+    /// </summary>
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        double cos = Math.Cos(angle);
+        double sin = Math.Sin(angle);
+
+        if (rows != 2 || rows != 2)
+        {
+            return new double[,] {{-1}};
+        }
+
+        double[,] result = new double[row, cols];
+        if (angle < 0)
+        {
+            result[0, 0] = cos * matrix[0, 0] + sin * matrix[0, 1];
+            result[0, 1] = -sin * matrix[0, 0] + cos * matrix[0, 1];
+            result[1, 0] = cos * matrix[1, 0] + sin * matrix[1, 1];
+            result[1, 1] = -sin * matrix[1, 0] + cos * matrix[1, 1];
+        }
+        else
+        {
+            result[0, 0] = cos * matrix[0, 0] + -sin * matrix[0, 1];
+            result[0, 1] = sin * matrix[0, 0] + cos * matrix[0, 1];
+            result[1, 0] = cos * matrix[1, 0] + -sin * matrix[1, 1];
+            result[1, 1] = sin * matrix[1, 0] + cos * matrix[1, 1];
+        }
+        return result;
+    }
+}
+
