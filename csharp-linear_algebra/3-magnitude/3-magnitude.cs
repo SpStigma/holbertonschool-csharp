db4fdce99624cc5d386d793a2d252d3b67e01c28
Rivet Yoann <GitHubStigma@gmail.com>
2024-05-27 13:53:50 +0200

    Fix: the operator to "&&".

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -4,16 +4,16 @@ class VectorMath
 {
     public static double Magnitude(double[] vector)
     {
-        if (vector.Length != 2 || vector.Length != 3)
+        if (vector.Length != 2 && vector.Length != 3)
         {
             return -1;
         }
 
         double result = 0;
 
-        foreach (double coordonates in vector)
+        foreach (double coordinate in vector)
         {
-            result += coordonates * coordonates;
+            result += coordinate * coordonate;
         }
 
         return Math.Round(Math.Sqrt(result), 2);
