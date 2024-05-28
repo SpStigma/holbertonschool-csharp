9670a0b4db72af7bda7c9dccba3e78c9398a1bfe
Rivet Yoann <GitHubStigma@gmail.com>
2024-05-28 15:00:07 +0200

    Go back to the old code.

--- csharp-linear_algebra/28-determinant/28-determinant.cs
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -30,9 +30,16 @@ public class MatrixMath
 
         if (rows == 3)
         {
-            return matrix[0, 0] * ((matrix[1, 1] * matrix[2, 2]) - (matrix[2, 1] * matrix[1, 2]))
-                    - matrix[0, 1] * ((matrix[1, 0] * matrix[2, 2])-(matrix[1, 2] * matrix[2, 0]))
-                    + matrix[0, 2] * ((matrix[1, 0] * matrix[2, 1])+(matrix[1, 1] * matrix[2, 0]));
+            double det = 0;
+
+            det = matrix[0, 0] * matrix[1, 1] * matrix[2, 2] +
+                  matrix[0, 1] * matrix[1, 2] * matrix[2, 0] +
+                  matrix[0, 2] * matrix[1, 0] * matrix[2, 1] -
+                  matrix[0, 2] * matrix[1, 1] * matrix[2, 0] -
+                  matrix[0, 1] * matrix[1, 0] * matrix[2, 2] -
+                  matrix[0, 0] * matrix[1, 2] * matrix[2, 1];
+
+            return det;
         }
 
         return -1;
