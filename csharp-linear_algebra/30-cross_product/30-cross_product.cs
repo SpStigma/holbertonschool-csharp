a030e06d166b4387b263dea9bb51d937802eb410
Rivet Yoann <GitHubStigma@gmail.com>
2024-05-28 17:05:29 +0200

    Add; Round to 2.

--- csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -17,9 +17,9 @@ class VectorMath
 
         double[] crossProduct = new double[vector1.Length];
 
-        crossProduct[0] = vector1[1] * vector2[2] - vector1[2] * vector2[1];
-        crossProduct[1] = vector1[2] * vector2[0] - vector1[0] * vector2[2];
-        crossProduct[2] = vector1[0] * vector2[1] - vector1[1] * vector2[0];
+        crossProduct[0] = Math.Round(vector1[1] * vector2[2] - vector1[2] * vector2[1], 2);
+        crossProduct[1] = Math.Round(vector1[2] * vector2[0] - vector1[0] * vector2[2], 2);
+        crossProduct[2] = Math.Round(vector1[0] * vector2[1] - vector1[1] * vector2[0], 2);
 
         return crossProduct;
     }
