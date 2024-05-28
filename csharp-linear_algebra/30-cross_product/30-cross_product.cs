85ceecc077ab6a286f2e1a3f77c2883380a475f4
Rivet Yoann <GitHubStigma@gmail.com>
2024-05-28 17:11:39 +0200

    Fix: Change the way to way to check if they are both 3D vector.

--- csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -10,16 +10,16 @@ class VectorMath
     /// </summary>
     public static double[] CrossProduct(double[] vector1, double[] vector2)
     {
-        if (vector1.Length != vector2.Length || vector1.Length != 2 || vector2.Length != 2)
+        if (vector1.Length != 3 || vector2.Length != 3)
         {
             return new double[] {-1};
         }
 
-        double[] crossProduct = new double[vector1.Length];
+        double[] crossProduct = new double[3];
 
-        crossProduct[0] = Math.Round(vector1[1] * vector2[2] - vector1[2] * vector2[1], 2);
-        crossProduct[1] = Math.Round(vector1[2] * vector2[0] - vector1[0] * vector2[2], 2);
-        crossProduct[2] = Math.Round(vector1[0] * vector2[1] - vector1[1] * vector2[0], 2);
+        crossProduct[0] = Math.Round((vector1[1] * vector2[2]) - (vector1[2] * vector2[1]), 2);
+        crossProduct[1] = Math.Round((vector1[2] * vector2[0]) - (vector1[0] * vector2[2]), 2);
+        crossProduct[2] = Math.Round((vector1[0] * vector2[1]) - (vector1[1] * vector2[0]), 2);
 
         return crossProduct;
     }
