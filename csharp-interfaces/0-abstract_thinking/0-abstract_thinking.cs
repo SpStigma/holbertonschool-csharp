1a132c67e4ca053db98346ac03c41e13b8071383
SpStigma <githubstigma@gmail.com>
2024-07-22 19:22:44 +0200

    Add Xml documentation

--- csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 
+
+/// <summary>
+/// Represent a bastract class Base.
+/// </summary>
 public abstract class Base
 {
     public string name { get; set; }
-
+    
+    /// <summary>
+    /// Returns a string representation of the object in the format "<name> is a <type>".
+    /// </summary>
     public override string ToString()
     {
         return $"{name} is a {GetType().Name}";
