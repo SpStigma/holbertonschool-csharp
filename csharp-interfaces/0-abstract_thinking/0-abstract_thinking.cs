99392ba553fc24d2422063f6cc65af5373109f7b
SpStigma <githubstigma@gmail.com>
2024-07-22 19:26:27 +0200

    Correction of my XML documentation.

--- csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 
-
 /// <summary>
-/// Represent a bastract class Base.
+/// Represents an abstract base class that provides a common name property and a custom string representation.
 /// </summary>
 public abstract class Base
 {
+    /// <summary>
+    /// Gets or sets the name of the object.
+    /// </summary>
     public string name { get; set; }
     
     /// <summary>
-    /// Returns a string representation of the object in the format "<name> is a <type>".
+    /// Returns a string representation of the object in the format.
     /// </summary>
     public override string ToString()
     {
         return $"{name} is a {GetType().Name}";
     }
-    
 }
