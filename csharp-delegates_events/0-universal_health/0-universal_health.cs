cb62d92f133a91a1a4f9a131c1529b023fe8c05d
Rivet Yoann <GitHubStigma@gmail.com>
2024-07-23 14:33:17 +0200

    Create Player class with constructor and method to print health.

---
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+/// <summary>
+/// Represent the Player class with 3 differents properties.
+/// </summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// <summary>
+    /// Constructor to initialize the properties name and maxHp and hp
+    /// </summary>
+    public Player(string name ="Player", float maxHp = 100)
+    {
+        this.name = name;
+
+        if (maxHp < 0)
+        {
+            this.maxHp = 100;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+        this.hp = maxHp;
+    }
+
+    /// <summary>
+    /// Method to Print Health.
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+}
