1702493ab82825b4896cefc54f5992ac4b55ce68
SpStigma <githubstigma@gmail.com>
2024-07-23 20:11:44 +0200

    Add enum and a delegate to modifier if a modificater is cjhosen.

---
+++ csharp-delegates_events/3-modified_behavior/3-modified_behavior.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+/// <summary>
+/// enum Modifier status.
+/// </summary>
+public enum Modifier
+{
+    /// <summary>
+    /// Weak status.
+    /// </summary>
+    Weak,
+    /// <summary>
+    /// Base status.
+    /// </summary>
+    Base,
+    /// <summary>
+    /// Strong Status.
+    /// </summary>
+    Strong
+}
+
+/// <summary>
+/// Delegate to calculate a modified value based on a base value and a modifier.
+/// </summary>
+public delegate float CalculateModifier(float baseValue, Modifier modifier);
+
+/// <summary>
+/// Represent the palyer class.
+/// </summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// <summary>
+    /// Constructor to initialize the properties name and maxHp and hp
+    /// </summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+
+        if (maxHp <= 0)
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+        this.hp = this.maxHp;
+    }
+
+    /// <summary>
+    /// Method to print health.
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+
+    /// <summary>
+    /// Method to apply damage to the player.
+    /// </summary>
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+        {
+            damage = 0;
+        }
+        Console.WriteLine($"{this.name} takes {damage} damage!");
+        float newHp = this.hp - damage;
+        ValidateHP(newHp);
+    }
+
+    /// <summary>
+    /// Method to heal the player.
+    /// </summary>
+    public void HealDamage(float heal)
+    {
+        if (heal < 0)
+        {
+            heal = 0;
+        }
+        Console.WriteLine($"{this.name} heals {heal} HP!");
+        float newHp = this.hp + heal;
+        ValidateHP(newHp);
+    }
+
+    /// <summary>
+    /// Method to Validate hp after Heal and TakeDamage.
+    /// </summary>
+    public void ValidateHP(float newHp)
+    {
+        if (newHp < 0)
+        {
+            this.hp = 0;
+        }
+        else if (newHp > this.maxHp)
+        {
+            this.hp = this.maxHp;
+        }
+        else
+        {
+            this.hp = newHp;
+        }
+    }
+
+    /// <summary>
+    /// Method to apply a modifier to a base value.
+    /// </summary>
+    public float ApplyModifier(float baseValue, Modifier modifier)
+    {
+        if (modifier == Modifier.Weak)
+        {
+            return baseValue / 2;
+        }
+        else if (modifier == Modifier.Base)
+        {
+            return baseValue;
+        }
+        else if (modifier == Modifier.Strong)
+        {
+            return baseValue * 1.5f;
+        }
+        return 0;
+    }
+}
