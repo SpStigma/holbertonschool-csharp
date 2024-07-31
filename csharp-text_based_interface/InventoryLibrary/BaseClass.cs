4854e5da03e4edc159bb3da59b4738944a61d706
Rivet Yoann <GitHubStigma@gmail.com>
2024-07-31 17:31:56 +0200

    Create a construcor in BaseClass.

--- csharp-text_based_interface/InventoryLibrary/BaseClass.cs
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -5,4 +5,11 @@ public class BaseClass
     public string id { get; set; }
     public DateTime date_created { get; set; }
     public DateTime date_updated { get; set; }
+
+    public BaseClass()
+    {
+        id = Guid.NewGuid().ToString();
+        date_created = DateTime.UtcNow;
+        date_updated = DateTime.UtcNow;
+    }
 }
