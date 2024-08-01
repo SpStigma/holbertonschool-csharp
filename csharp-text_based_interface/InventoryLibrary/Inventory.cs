5d88119136f6e95fb9f0b0cecef4b489e4ba0506
SpStigma <githubstigma@gmail.com>
2024-08-01 18:50:09 +0200

    Modify the name of the files for the checker.

--- csharp-text_based_interface/InventoryLibrary/Inventory.cs
+++ csharp-text_based_interface/InventoryLibrary/Inventory.cs
@@ -1,70 +1,28 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text.Json;
 
-public class JSONStorage
+public class Inventory : BaseClass
 {
-    public Dictionary<string, string> objects =  new Dictionary<string, string>();
+    public string user_id { get; set; }
+    public string item_id { get; set; }
+    public int quantity;
 
-    public DictionartAll()
+    public int quantity
     {
-        return objects;
-    }
-
-    public void New(object obj)
-    {
-        string className = obj.GetType().Name;
-        string id = obj.GetType().GetProperty("id")?.GetValue(obj)?.ToString();
-
-                if (id != null)
+        get => quantity;
+        set
         {
-            string key = $"{className}.{id}";
-
-            string jsonString = JsonSerializer.Serialize(obj);
-
-            objects[key] = jsonString;
-        }
-        else
-        {
-            throw new ArgumentException("The object must have an 'id' property");
+            if(value < 0)
+            {
+                throw new ArgumentException("can not be negative");
+            }
+            quantity = value;
         }
     }
 
-    public Save()
+    public Inventory(string UserId, string ItemId, int Quantity = 1)
     {
-        string directory = "storage"
-
-        // Use .IO to check if directory exist
-        if(!Directory.Exists(directory))
-        {
-            // if not create it
-            Directory.CreateDirectory(directory);
-        }
-        //name of the file we want to create
-        string fileName = "inventory_manager.json";
-        //Create the path
-        string filepath = Path.Combine(directory, fileName);
-        //Transform to Json
-        string jsonString = JsonSerializer.Serialize(objects);
-        //Create the file with json inside.
-        fileName.WriteAllText(filepath, jsonString);
-    }
-
-    public void Load()
-    {
-        string directoryPath = "storage";
-        string fileName = "inventory_manager.json";
-        string filePath = Path.Combine(directoryPath, fileName);
-
-        if (File.Exists(filePath))
-        {
-            string jsonString = File.ReadAllText(filePath);
-            objects = JsonSerializer.Deserialize<MyObject[]>(jsonString);
-        }
-        else
-        {
-            Console.WriteLine("Le fichier JSON n'existe pas.");
-        }
+        this.user_id = UserId;
+        this.item_id = ItemId;
+        this.quantity = Quantity;
     }
 }
